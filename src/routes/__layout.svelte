7aed1a52232bc7e4e1a3e82eaf30840d020d0e25
mirkoschubert <office@mirkoschubert.de>
2022-02-26 14:18:19 +0100

    Sass, PostCSS & Kit Config

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import '../global/sass/_layout.sass'
+  import '../global/sass/main.sass'
+
+  //export let key
+	//let main
+</script>
+
+<div class="wrapper">
+  <slot />
+</div>
+
+<style lang="sass" global>
+	.wrapper
+		overflow-y: auto
+		-webkit-overflow-scrolling: touch
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,47 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		sass: {
+			includePaths: ['src'],
+      prependData: '@import "src/global/sass/base/_variables.sass"',
+      renderSync: true
+		},
+    postcss: {
+			plugins: [
+        cssnano({ preset: 'cssnano-preset-default' }),
+        autoprefixer({})
+      ]
+		},
+  }),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    target: '#app',
+    files: {
+			assets: 'static',
+			lib: 'src/lib',
+			routes: 'src/routes',
+			serviceWorker: 'src/service-worker',
+			template: 'src/app.html',
+			hooks: 'src/hooks'
+		},
+    vite: {
+			resolve: {
+				alias: {
+					$stores: path.resolve('./src/lib/stores'),
+					//$api: path.resolve('./src/routes/api'),
+					$components: path.resolve('./src/lib/components')
+				}
+			},
+		}
 	}
 };
 
